9ef7554e9b103e1a9f91a251f743f64a55054c90
Amatrasu66 <135813973+Amatrasu66@users.noreply.github.com>
2025-09-14 19:51:12 +0530

    Update encrypter and decripter.py

--- encrypter and decripter.py
+++ encrypter and decripter.py
@@ -2,15 +2,24 @@ dict1 = {"a": "q","b" : "w","c" : "e","d" : "r","e" : "t","f" : "y","g" : "u","h
 }
 
 lst = []
+decriptedlst = []
 
 valu = input("Enter the value : ")
 
-occurance = True
 
-while occurance:
-    for val in dict1:
-        if("." in valu):
-            occurance = False
-        elif(valu == dict1.get(val)):
-            lst.append(dict1.get(valu))
-    print(lst)
+for char in valu.lower():
+    if char in dict1:
+        lst.append(dict1.get(char))
+print(lst)
+tup = tuple(lst)
+print(tup)
+
+question = input("do you want to Decript it yeah or Nah : ")
+
+if question.lower() in ["yeah", "yes"]:
+    reversedict = {value: key for key, value in dict1.items()}
+    
+    for cha in lst:
+        if cha in reversedict:
+            decriptedlst.append(reversedict[cha])
+print(decriptedlst)
