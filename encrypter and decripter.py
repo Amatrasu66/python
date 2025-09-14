6997e1ecf86c5871b5c6b0a9558156363f0c8e25
Amatrasu66 <135813973+Amatrasu66@users.noreply.github.com>
2025-09-14 15:23:07 +0530

    Update encrypter and decripter.py

--- encrypter and decripter.py
+++ encrypter and decripter.py
@@ -1,4 +1,3 @@
-#encrypterr and decripter
 dict1 = {"a": "q","b" : "w","c" : "e","d" : "r","e" : "t","f" : "y","g" : "u","h" : "i","i" : "o","j" : "p","k" : "a","l" : "s","m" : "d","n" : "f","o" : "g","p" : "h","q" : "j","r" : "k","s" : "l","t" : "z","u" : "x","v" : "c","w" : "v","x" : "b","y" : "n","z" : "m",
 }
 
@@ -6,7 +5,12 @@ lst = []
 
 valu = input("Enter the value : ")
 
-for val in dict1:
-    if(valu == dict1.get(val)):
-        lst.append(dict1.get(valu))
-print(lst)
+occurance = True
+
+while occurance:
+    for val in dict1:
+        if("." in valu):
+            occurance = False
+        elif(valu == dict1.get(val)):
+            lst.append(dict1.get(valu))
+    print(lst)
