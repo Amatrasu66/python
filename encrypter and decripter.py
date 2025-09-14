9732435aa608b2e98e7f615a31485d14fe987aef
Amatrasu66 <135813973+Amatrasu66@users.noreply.github.com>
2025-09-14 14:43:21 +0530

    Create encrypter and decripter.py

---
+++ encrypter and decripter.py
@@ -0,0 +1,12 @@
+#encrypterr and decripter
+dict1 = {"a": "q","b" : "w","c" : "e","d" : "r","e" : "t","f" : "y","g" : "u","h" : "i","i" : "o","j" : "p","k" : "a","l" : "s","m" : "d","n" : "f","o" : "g","p" : "h","q" : "j","r" : "k","s" : "l","t" : "z","u" : "x","v" : "c","w" : "v","x" : "b","y" : "n","z" : "m",
+}
+
+lst = []
+
+valu = input("Enter the value : ")
+
+for val in dict1:
+    if(valu == dict1.get(val)):
+        lst.append(dict1.get(valu))
+print(lst)
